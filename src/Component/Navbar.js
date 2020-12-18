b0b2b70318cc9b6488d147dc455d21c9b8717910
boyhp0077 <boyhp0077@gmail.com>
2020-12-18 23:42:23 +0700

    first commit

---
+++ src/Component/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styling/navbar.scss'
+
+export default function Navbar() {
+    return (
+        <nav class="navbar">
+            <div className="navbar__container">
+                <a href="#home" className="navbar__logo">COLOR</a>
+            </div>
+        </nav>
+    )
+}
